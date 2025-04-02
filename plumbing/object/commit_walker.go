0eb440f4aff09741a6c0bd13c19ddb193bba94c9
Adrian Edwards <adrian@adriancedwards.com>
2025-04-01 22:01:15 -0400

    attempt fix

--- plumbing/object/commit_walker.go
+++ plumbing/object/commit_walker.go
@@ -220,11 +220,11 @@ func (w *commitPostIteratorNM) Next() (*Commit, error) {
 		}
 
 		w.seen[c.Hash] = true
-		// TODO: change this
-		return c, c.Parents().ForEach(func(p *Commit) error {
+		if p, err := c.Parent(0); err != nil {
 			w.stack = append(w.stack, p)
-			return nil
-		})
+		}
+
+		return c, nil
 	}
 }
 
